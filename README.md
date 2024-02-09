29b5ebeb46d2d3011840d467afc73da497a9153c
Mikhail Laukhin <lauhinmihail@yandex.ru>
2024-02-09 09:21:50 +0300

    Uhhh... The game is ready

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 [![Actions Status](https://github.com/LaukhinMikhail/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/LaukhinMikhail/php-project-45/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/30ec7ff363754fcc95a9/maintainability)](https://codeclimate.com/github/LaukhinMikhail/php-project-45/maintainability)
+
+### Usage cases:
+Try to make install in project:
+https://asciinema.org/a/wTFIOnhv1XDchQLEgJcGhtn24
+
+First try the game (failed):
+https://asciinema.org/a/fP4F6lnG2FuNPTnzl7d1M0vsM
+
+Successful game attempt:
+https://asciinema.org/a/WltFrk4mTTvzqtagA2vCG3zZ5
--- src/Cli.php
+++ src/Cli.php
@@ -2,7 +2,31 @@
 
 namespace BrainGames\Cli;
 
-echo "May I have your name?\n";
+function game()
+{
+    echo "May I have your name?\n";
 
-$name = trim(fgets(STDIN));
-echo "Hello, {$name}!\n";
+    $name = trim(fgets(STDIN));
+    echo "Hello, {$name}!\n";
+    
+    print_r ("Welcome to the Brain Games!
+Answer \"yes\" if the number is even, otherwise answer \"no\".
+\n\n");
+
+    $trueCount = 0;
+    $movesNum = 3;
+
+    while ($trueCount < $movesNum) {
+        $randomNumber = rand(1, 99);
+        $currentResponse = $randomNumber % 2 === 0 ? 'Yes' : 'No';
+        print_r ("Question: " . $randomNumber . "\n" . "Your answer: " . "\n");
+
+        $response = trim(fgets(STDIN));
+        if ($response != $currentResponse) {
+            return print_r("\"{$response}\" is wrong answer ;(. Correct answer was \"{$currentResponse}\".\nLet's try again, {$name}!\n");
+        }
+
+    $trueCount++;
+    }
+    print_r ("Congratulations, " . $name . "!\n");
+}
