409be28ae3befa34ed8c1e95330ae07600708ee9
Mikhail Laukhin <lauhinmihail@yandex.ru>
2024-02-21 15:12:54 +0300

    Removed most of the code repetition in the project

--- src/Engine.php
+++ src/Engine.php
@@ -1,6 +1,33 @@
 <?php
 
-namespace BrainGames\Engine;
+//namespace BrainGames\Engine;
+
+
+function isResponseCorrect($actualResponse, $currentResponse)
+{
+    if ($actualResponse == $currentResponse) {
+        //print_r("Current!\n");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function showFailMessage($name, $actualResponse, $currentResponse)
+{
+    return print_r("\"{$actualResponse}\" is wrong answer ;(. Correct answer was \"{$currentResponse}\".\nLet's try again, {$name}!\n");
+}
+
+function showSuccesfulGameEnding($name)
+{
+    return print_r("Congratulations, " . $name . "!\n");
+}
+
+function getResponse()
+{
+    return trim(fgets(STDIN));
+}
 
 function greeting()
 {
@@ -12,12 +39,56 @@ function greeting()
     return $name;
 }
 
-function gcd($num1, $num2)
+function getMaximumMovesNumber()
 {
-    $gcd = $num1 <= $num2 ? $num1 : $num2;
-    while (!($num1 % $gcd === 0 && $num2 % $gcd === 0)) {
+    return 3;
+}
+
+function generateRemainderOfDivision()
+{
+    $randomNumber = rand(1, 99);
+    print_r ("Question: " . $randomNumber . "\n" . "Your answer: " . "\n");
+    $currentReponse = $randomNumber % 2 === 0 ? 'Yes' : 'No';
+    return $currentReponse;
+}
+
+function generateCalc()
+{
+    $operands = ['+', '-', '*'];
+    $randomNumber1 = rand(1, 99);
+    $randomNumber2 = rand(1, 99);
+    $numOperand = rand(0, 2);
+    switch ($numOperand) {
+        case 0:
+            $currentResponse = $randomNumber1 + $randomNumber2;
+            break;
+        case 1:
+            $currentResponse = $randomNumber1 - $randomNumber2;
+            break;
+        case 2:
+            $currentResponse = $randomNumber1 * $randomNumber2;
+            break;
+    }
+
+    print_r ("Question: " . $randomNumber1 . $operands[$numOperand] . $randomNumber2 . ". Your answer: " . "\n");
+    return $currentResponse;
+}
+
+function generateGCD()
+{
+    $randomNumber1 = rand(1, 99);
+    $randomNumber2 = rand(1, 99);
+
+    print_r ("Question: " . $randomNumber1 . " " . $randomNumber2 . ". Your answer: " . "\n");
+
+    $gcd = $randomNumber1 <= $randomNumber2 ? $randomNumber1 : $randomNumber2; //Находим правильный ответ и возвращаем
+    while (!($randomNumber1 % $gcd === 0 && $randomNumber2 % $gcd === 0)) {
         $gcd--;
-        //print_r ("Первое число - {$num1}, второе число - {$num2}, делитель - ${gcd}\n");
     }
     return $gcd;
+}
+
+function generateProgression()
+{
+
 }
