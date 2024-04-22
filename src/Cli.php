bd92ee6e2dfa9631eaae7aa4a4f6a01a1d78a338
Mikhail Laukhin <lauhinmihail@yandex.ru>
2024-04-22 17:31:05 +0300

    Remove autoload from Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,18 +5,6 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-// Путь который будет использован при глобальной установке пакета
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-
-// Путь для локальной работы с проектом
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 function getName()
 {
     line('Welcome to the Brain Game!');
