7ac04e5c02927112c2a927c8cd20708e2e629a7a
Mikhail Laukhin <lauhinmihail@yandex.ru>
2024-04-23 11:06:57 +0300

    Fix Cli.php "Welcome to the brain Game" ==> "Welcome to the Brain Games"

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function getName()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt("May I have your name?\n", false, "");
     line("Hello, %s!", $name);
     return $name;
