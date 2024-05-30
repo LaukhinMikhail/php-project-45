25703c1aefb16a8ddb4d29ede1e4eb32f5ee2199
Mikhail Laukhin <lauhinmihail@yandex.ru>
2024-05-30 11:22:36 +0300

    Now the calculate function uses the operand itself instead of its number in the array

--- src/Games/BrainCalc.php
+++ src/Games/BrainCalc.php
@@ -8,23 +8,22 @@ function getCalc()
     $operands = ['+', '-', '*'];
     $randomNumber1 = rand(1, 99);
     $randomNumber2 = rand(1, 99);
-    $numOperand = rand(0, 2);
-    $currentResponse = calculate($randomNumber1, $randomNumber2, $numOperand);
-    $currentOperand = $operands[$numOperand];
-    $result['question'] = "{$randomNumber1} {$currentOperand} {$randomNumber2}.";
+    $operand = $operands[rand(0, 2)];
+    $currentResponse = calculate($randomNumber1, $randomNumber2, $operand);
+    $result['question'] = "{$randomNumber1} {$operand} {$randomNumber2}.";
     $result['response'] = $currentResponse;
     return $result;
 }
 function calculate($num1, $num2, $operand)
 {
     switch ($operand) {
-        case 0:
+        case '+':
             $result = $num1 + $num2;
             break;
-        case 1:
+        case '-':
             $result = $num1 - $num2;
             break;
-        case 2:
+        case '*':
             $result = $num1 * $num2;
             break;
     }
